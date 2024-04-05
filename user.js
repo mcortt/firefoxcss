4f3b1d4ab66179b3b138df17276216317a3bb74b
Matthew Cody Ortt <9371806+mcortt@users.noreply.github.com>
2024-04-05 01:49:46 -0400

    Update user.js
    allow telemetry and crash reports

--- user.js
+++ user.js
@@ -135,23 +135,23 @@ user_pref("permissions.manager.defaultsUrl", "");
 user_pref("webchannel.allowObject.urlWhitelist", "");
 
 /** TELEMETRY ***/
-user_pref("datareporting.policy.dataSubmissionEnabled", false);
-user_pref("datareporting.healthreport.uploadEnabled", false);
-user_pref("toolkit.telemetry.unified", false);
-user_pref("toolkit.telemetry.enabled", false);
-user_pref("toolkit.telemetry.server", "data:,");
-user_pref("toolkit.telemetry.archive.enabled", false);
-user_pref("toolkit.telemetry.newProfilePing.enabled", false);
-user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
-user_pref("toolkit.telemetry.updatePing.enabled", false);
-user_pref("toolkit.telemetry.bhrPing.enabled", false);
-user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
-user_pref("toolkit.telemetry.coverage.opt-out", true);
-user_pref("toolkit.coverage.opt-out", true);
-user_pref("toolkit.coverage.endpoint.base", "");
-user_pref("browser.ping-centre.telemetry", false);
-user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
-user_pref("browser.newtabpage.activity-stream.telemetry", false);
+// user_pref("datareporting.policy.dataSubmissionEnabled", false);
+// user_pref("datareporting.healthreport.uploadEnabled", false);
+// user_pref("toolkit.telemetry.unified", false);
+// user_pref("toolkit.telemetry.enabled", false);
+// user_pref("toolkit.telemetry.server", "data:,");
+// user_pref("toolkit.telemetry.archive.enabled", false);
+// user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+// user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+// user_pref("toolkit.telemetry.updatePing.enabled", false);
+// user_pref("toolkit.telemetry.bhrPing.enabled", false);
+// user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+// user_pref("toolkit.telemetry.coverage.opt-out", true);
+// user_pref("toolkit.coverage.opt-out", true);
+// user_pref("toolkit.coverage.endpoint.base", "");
+// user_pref("browser.ping-centre.telemetry", false);
+// user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+// user_pref("browser.newtabpage.activity-stream.telemetry", false);
 
 /** EXPERIMENTS ***/
 user_pref("app.shield.optoutstudies.enabled", false);
@@ -159,9 +159,9 @@ user_pref("app.normandy.enabled", false);
 user_pref("app.normandy.api_url", "");
 
 /** CRASH REPORTS ***/
-user_pref("breakpad.reportURL", "");
-user_pref("browser.tabs.crashReporting.sendReport", false);
-user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+// user_pref("breakpad.reportURL", "");
+// user_pref("browser.tabs.crashReporting.sendReport", false);
+// user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 
 /** DETECTION ***/
 user_pref("captivedetect.canonicalURL", "");
@@ -246,15 +246,13 @@ user_pref("gfx.canvas.accelerated", true);
 user_pref("permissions.default.shortcuts", 2);
 // PREF: open bookmarks in new tabs always
 user_pref("browser.tabs.loadBookmarksInTabs", true);
-// PREF: enable fingerprinting resistance
+// PREF: enable fingerprinting resistance/protection
 // user_pref("privacy.resistFingerprinting", true);
-// user_pref("privacy.fingerprintingProtection", true);
+user_pref("privacy.fingerprintingProtection", true);
 // PREF: disable ALT key bringing up titlebar.
 user_pref("ui.key.menuAccessKeyFocuses", false);
 // PREF: enable tab previews
 user_pref("browser.tabs.cardPreview.enabled", true);
-// PREF: set default apperance to Dark
-// user_pref("layout.css.prefers-color-scheme.content-override", 0);
 /****************************************************************************
  * SECTION: SMOOTHFOX                                                       *
 ****************************************************************************/
